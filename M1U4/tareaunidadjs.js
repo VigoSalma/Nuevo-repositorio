fb9b593be7f308bd053475f78a8d35258bfed836
Vigo Salma Roxana <salmavigookk@gmail.com>
2022-04-22 16:47:41 -0300

    ejercicio 2

---
+++ M1U4/tareaunidadjs.js
@@ -0,0 +1,16 @@
+var j = ['1', '56', '151', '1164', '21', '151', '19', '185', '1555']
+var cuantos = j.length
+document.write (cuantos)
+var x = 0
+
+var mayor = 0
+for (x = 0; x = cuantos; x++) {
+    if (j[x] >= mayor) {
+        mayor = j[x]
+    }
+    else {
+        mayor = mayor
+    }
+
+}
+//document.write('el mayor numero es ' + mayor)
